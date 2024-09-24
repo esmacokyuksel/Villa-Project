a5c54e07d3f41bc4e27aea287ce462d31e2d73ea
Esma Çokyüksel <125696026+esmacokyuksel@users.noreply.github.com>
2024-09-24 22:17:39 +0300

    Add files via upload

---
+++ Villa.Business/Concrete/ContactManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Business.Abstract;
+using Villa.Entity.Entities;
+
+namespace Villa.Business.Concrete
+{
+    public class ContactManager : GenericManager<Contact>, IContactService
+    {
+        public ContactManager(DataAccess.Abstract.IGenericDal<Contact> genericDal) : base(genericDal)
+        {
+        }
+    }
+}
---
+++ Villa.Business/Validations/QuestionValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Entity.Entities;
+
+namespace Villa.Business.Validations
+{
+    public  class QuestionValidator:AbstractValidator<Quest>
+    {
+        public QuestionValidator()
+        {
+          
+            RuleFor(x=>x.Question).NotEmpty().WithMessage("Soru boş bırakılamaz");
+            RuleFor(x => x.Question).MinimumLength(10).WithMessage("Soru en az 10 karakterden oluşmalıdır");
+            RuleFor(x => x.Question).MaximumLength(300).WithMessage("Soru en fazla 200 karakterden oluşmalıdır");
+            RuleFor(x => x.Answer).NotEmpty().WithMessage("Cevap boş bırakılamaz");
+            RuleFor(x => x.Answer).MinimumLength(10).WithMessage("Cevap en az 20 karakterden oluşmalıdır");
+            RuleFor(x => x.Answer).MaximumLength(300).WithMessage("Cevap en fazla 300 karakterden oluşmalıdır");
+
+        }
+    }
+}
