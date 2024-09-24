60578b08ec4356639a7d73a73e6bcd83c2063e1c
Esma Çokyüksel <125696026+esmacokyuksel@users.noreply.github.com>
2024-09-24 22:14:59 +0300

    Add files via upload

---
+++ ViewComponents/AdminLayout/_AdminPreloader.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.WebUI.Views.Shared.Components._AdminHead
+{
+    public class _AdminPreloader : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/DefaultIndex/_DefaultMessage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Villa.WebUI.ViewComponents.DefaultIndex
+{
+    public class _DefaultMessage : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
